af7d0c1b2b379c370960bd38e972db2d9eb47c93
Mass23 <33321080+Mass23@users.noreply.github.com>
2019-04-10 10:25:31 +0200

    Create transcribe_dna.cpp

---
+++ transcribe_dna.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+using namespace std;
+
+int main(int argc, char **argv) {
+
+    std::ifstream  fin(argv[1]);
+    std::string    line;
+
+    cout << "-------------------------\n";
+    cout << "     Transcribe DNA:    \n";
+    cout << "-------------------------\n";
+
+    while(std::getline(fin, line)) {
+
+        if(line.empty()){
+          continue;
+        }
+
+        else if (line[0] == '>'){
+          cout << "Sequence: " << line.erase(0, 1) << "\n";
+        }
+
+        else {
+          for(int i = 0; i < line.length(); i++){
+
+            if (line[i] == 'T'){
+              cout << 'U';
+            }
+
+            else {
+              cout << line[i];
+            }
+
+          }
+        }
+
+        }
+
+    cout << '\n';
+    return 0;
+}
