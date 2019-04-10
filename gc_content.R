c71ba153e8052be5927964baebdeb0e862d51c1c
Mass23 <33321080+Mass23@users.noreply.github.com>
2019-04-10 15:18:11 +0200

    Create gc_content.R

---
+++ gc_content.R
@@ -0,0 +1,29 @@
+GCcontent <- function(sequence){
+  acount = 0
+  ccount = 0
+  gcount = 0
+  tcount = 0
+  
+  seq_split <- strsplit(sequence, "")[[1]]
+  
+  for (i in seq_split){
+    
+    if (i == 'A'){
+      acount = acount + 1
+    }
+    
+    else if (i == 'C'){
+      ccount = ccount + 1
+    }
+    
+    else if (i == 'G'){
+      gcount = gcount + 1
+    }
+    
+    else if (i == 'T'){
+      tcount = tcount + 1
+    }
+  }
+  return((gcount + ccount) / (acount + ccount + gcount + tcount))
+}
+GCcontent(seq)
